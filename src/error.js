311f43d507976f5cc310a78688421bf5858fc2e5
Anish Sapkota <sapkotaanish000@gmail.com>
2021-01-21 22:25:45 +0545

    initial commit

---
+++ src/error.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Error() {
+  return (
+    <div className="App">
+      <h2>Not Found!</h2>
+    </div>
+  );
+}
+
+export default Error;
